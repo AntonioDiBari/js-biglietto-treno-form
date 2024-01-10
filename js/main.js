4b96e46b86270e2fb5ef9174777d41ac042e555e
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-10 16:26:56 +0100

    feat: script 1° milestone

--- js/main.js
+++ js/main.js
@@ -1,45 +1,56 @@
-// //INIZIALIZZAZIONE COSTANTI
-// const resultPrice = document.getElementById("prezzoBiglietto");
+//INIZIALIZZAZIONE COSTANTI
+const resultPrice = document.getElementById("prezzo-biglietto");
 
-// const tariffa = 0.21;
-// console.log("Tariffa €" + tariffa);
+const tariffa = 0.21;
+console.log("Tariffa €" + tariffa);
 
-// const scontoUnder = 20;
-// console.log("Sconto Under 18 %" + scontoUnder);
+const scontoUnder = 20;
+console.log("Sconto Under 18 %" + scontoUnder);
 
-// const scontoOver = 40;
-// console.log("Sconto Over 65 %" + scontoOver);
+const scontoOver = 40;
+console.log("Sconto Over 65 %" + scontoOver);
 
-// let prezzo;
-// //INIZIALIZZAZIONE COSTANTI TRAMITE PROMPT ALL'UTENTE + RELATIVI CONTROLLI
+let prezzo;
+//INIZIALIZZAZIONE COSTANTI TRAMITE INPUT DALL'UTENTE + RELATIVI CONTROLLI
 
-// const distanza = prompt(
-//   "Quanti km vorreste percorrere con la nostra compagnia ferroviaria?",
-//   "30"
-// ).trim();
-// console.log("Distanza inserita km" + distanza);
+const nameInput = document.querySelector("#user-name");
 
-// const age = parseInt(prompt("Quanti anni ha ?", "24").trim());
-// console.log("Anni inseriti " + age);
+const distanzaInput = document.querySelector("#distanza");
 
-// if (isNaN(distanza) || isNaN(age) || distanza < 0 || age < 1 || age > 110) {
-//   alert("Dati inseriti errati!!");
-// } else {
-//   //CREAZIONE DEL PREZZO
-//   prezzo = tariffa * distanza;
-//   console.log("Prezzo lordo €" + prezzo);
-//   // APPLICAZIONE DEGLI SCONTI
-//   if (age < 19) {
-//     prezzo = prezzo - (prezzo * scontoUnder) / 100;
-//     console.log("Prezzo scontato €" + prezzo);
-//   } else if (age > 64) {
-//     prezzo = prezzo - (prezzo * scontoOver) / 100;
-//     console.log("Prezzo scontato €" + prezzo);
-//   }
-// }
-// // ARROTONDAMENTO E PARTE DECIMALE INTERESSATA
-// prezzo = prezzo.toFixed(2);
-// console.log("Prezzo finale €" + prezzo);
+const ageRangeInput = document.querySelector("#age-range");
 
-// //STAMPO IN OUTPUT IL PREZZO FINALE TRAMITE ELEMENTO HMTL
-// resultPrice.innerText = "Il prezzo del tuo biglietto è: € " + prezzo;
+const createButton = document.getElementById("create");
+
+createButton.addEventListener("click", function () {
+  const user = nameInput.value.trim();
+  console.log("Nome utente: " + user);
+
+  const distanza = distanzaInput.value.trim();
+  console.log("Distanza inserita km " + distanza);
+
+  const ageRange = ageRangeInput.value;
+  console.log("La fascia d'età inserita è " + ageRange);
+
+  if (distanza <= 0) {
+    alert("Dati inseriti errati!!");
+  } else {
+    //CREAZIONE DEL PREZZO
+    prezzo = tariffa * distanza;
+    console.log("Prezzo lordo €" + prezzo);
+    // APPLICAZIONE DEGLI SCONTI
+    if (ageRange == "minorenne") {
+      prezzo = prezzo - (prezzo * scontoUnder) / 100;
+      console.log("Prezzo scontato €" + prezzo);
+    } else if (ageRange == "over65") {
+      prezzo = prezzo - (prezzo * scontoOver) / 100;
+      console.log("Prezzo scontato €" + prezzo);
+    }
+  }
+
+  // ARROTONDAMENTO E PARTE DECIMALE INTERESSATA
+  prezzo = prezzo.toFixed(2);
+  console.log("Prezzo finale €" + prezzo);
+
+  //STAMPO IN OUTPUT IL PREZZO FINALE TRAMITE ELEMENTO HMTL
+  resultPrice.innerText = "Il prezzo del tuo biglietto è: € " + prezzo;
+});
